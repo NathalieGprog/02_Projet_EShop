fd961032c4b5557d4f16391f460df5981880210a
INTI2017FORMATION <INTI2017FORMATION@10.33.12.30>
2017-08-06 14:21:23 +0200

    Rudy : commit 06/08 14:20 : Couche Model et baseDonnées.properties

---
+++ 01_EshopV2/src/main/java/fr/adaming/model/Client.java
@@ -0,0 +1,164 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_client")
+	private int idClient;
+	@Column(name = "nom_client")
+	private String nomClient;
+	private String adresse;
+	private String email;
+	private String tel;
+
+	@OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+	private List<Commande> lCommandes;
+
+	/**
+	 * Constructeur vide
+	 */
+	public Client() {
+		super();
+	}
+
+	/**
+	 * @param nomClient
+	 * @param adresse
+	 * @param email
+	 * @param tel
+	 */
+	public Client(String nomClient, String adresse, String email, String tel) {
+		super();
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	/**
+	 * @param idClient
+	 * @param nomClient
+	 * @param adresse
+	 * @param email
+	 * @param tel
+	 */
+	public Client(int idClient, String nomClient, String adresse, String email, String tel) {
+		super();
+		this.idClient = idClient;
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	/**
+	 * @return the idClient
+	 */
+	public int getIdClient() {
+		return idClient;
+	}
+
+	/**
+	 * @param idClient
+	 *            the idClient to set
+	 */
+	public void setIdClient(int idClient) {
+		this.idClient = idClient;
+	}
+
+	/**
+	 * @return the nomClient
+	 */
+	public String getNomClient() {
+		return nomClient;
+	}
+
+	/**
+	 * @param nomClient
+	 *            the nomClient to set
+	 */
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+	/**
+	 * @param adresse
+	 *            the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the tel
+	 */
+	public String getTel() {
+		return tel;
+	}
+
+	/**
+	 * @param tel
+	 *            the tel to set
+	 */
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	/**
+	 * @return the lCommandes
+	 */
+	public List<Commande> getlCommandes() {
+		return lCommandes;
+	}
+
+	/**
+	 * @param lCommandes
+	 *            the lCommandes to set
+	 */
+	public void setlCommandes(List<Commande> lCommandes) {
+		this.lCommandes = lCommandes;
+	}
+
+}
---
+++ 01_EshopV2/src/main/java/fr/adaming/model/Produit.java
@@ -0,0 +1,216 @@
+package fr.adaming.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produits")
+public class Produit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_produit")
+	private int idProduit;
+	private String designation;
+	private String description;
+	private double prix;
+	private int quantite;
+	@Column(nullable = false, columnDefinition = "TINYINT", length = 1)
+	private boolean selectionne;
+
+	@ManyToOne
+	@JoinColumn(name = "categorie_id", referencedColumnName = "id_categorie")
+	private Categorie categorie;
+
+	@OneToMany(mappedBy = "produit", cascade = CascadeType.ALL)
+	private List<LigneCommande> lLignesCommandes;
+
+	/**
+	 * Constructeur vide
+	 */
+	public Produit() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @param designation
+	 * @param description
+	 * @param prix
+	 * @param quantite
+	 * @param selectionne
+	 */
+	public Produit(String designation, String description, double prix, int quantite, boolean selectionne) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+	}
+
+	/**
+	 * 
+	 * @param idProduit
+	 * @param designation
+	 * @param description
+	 * @param prix
+	 * @param quantite
+	 * @param selectionne
+	 */
+	public Produit(int idProduit, String designation, String description, double prix, int quantite,
+			boolean selectionne) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+	}
+
+	/**
+	 * @return the idProduit
+	 */
+	public int getIdProduit() {
+		return idProduit;
+	}
+
+	/**
+	 * @param idProduit
+	 *            the idProduit to set
+	 */
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	/**
+	 * @return the designation
+	 */
+	public String getDesignation() {
+		return designation;
+	}
+
+	/**
+	 * @param designation
+	 *            the designation to set
+	 */
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the prix
+	 */
+	public double getPrix() {
+		return prix;
+	}
+
+	/**
+	 * @param prix
+	 *            the prix to set
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	/**
+	 * @return the quantite
+	 */
+	public int getQuantite() {
+		return quantite;
+	}
+
+	/**
+	 * @param quantite
+	 *            the quantite to set
+	 */
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	/**
+	 * @return the selectionne
+	 */
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+
+	/**
+	 * @param selectionne
+	 *            the selectionne to set
+	 */
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+
+	/**
+	 * @return the lLignesCommandes
+	 */
+	public List<LigneCommande> getlLignesCommandes() {
+		return lLignesCommandes;
+	}
+
+	/**
+	 * @param lLignesCommandes
+	 *            the lLignesCommandes to set
+	 */
+	public void setlLignesCommandes(List<LigneCommande> lLignesCommandes) {
+		this.lLignesCommandes = lLignesCommandes;
+	}
+
+	/**
+	 * @return the categorie
+	 */
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	/**
+	 * @param categorie
+	 *            the categorie to set
+	 */
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	// /* (non-Javadoc)
+	// * @see java.lang.Object#toString()
+	// */
+	// @Override
+	// public String toString() {
+	// return "Produit [designation=" + designation + ", description=" +
+	// description + ", prix=" + prix + ", quantite="
+	// + quantite + ", selectionne=" + selectionne + ", categorie=" + categorie
+	// + ", lLignesCommandes="
+	// + lLignesCommandes + "]";
+	// }
+	//
+	//
+
+}
